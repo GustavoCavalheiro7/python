9aa75b26b9fef25697342e596c6d68ad85532b25
Gustavo de Assis Cavalheiro <gustavocavalheiro0705@gmail.com>
2024-10-18 02:52:26 +0100

    Add files via upload

---
+++ quadrilatero.py
@@ -0,0 +1,8 @@
+#quadrado
+b=int(input("Digite a base do quadrilatero "))
+h=int(input("Digite a altura do quadrilatero "))
+
+if(b==h):
+    print("é um quadrado")
+else:
+    print("é um retangulo")
